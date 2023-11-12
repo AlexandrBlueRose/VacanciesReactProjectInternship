8d82bada02360aeb9c8302a4b80440545fd6ed08
Alexandr <mr.snow3@yandex.ru>
2023-11-13 01:47:36 +0300

    #115066 Добавлен и настроен начальный пустой проект

---
+++ src/components/AppProviders/AppProviders.tsx
@@ -0,0 +1,31 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactNode, useState } from 'react';
+
+interface AppProvidersProps {
+    children: ReactNode;
+}
+
+const AppProviders = ({ children }: AppProvidersProps) => {
+    const [queryClient] = useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    queries: {
+                        staleTime: 60 * 5 * 1000,
+                        retry: 0,
+                        refetchOnWindowFocus: process.env.NODE_ENV === 'production',
+                    },
+                },
+            })
+    );
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
+};
+
+export default AppProviders;
